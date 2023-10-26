b4418f74f4464b3491935e30490ebd8c926a4dd0
Thomas66 <thomassalvaterra2001@gmail.com>
2023-10-26 16:59:56 +0200

    first version

---
+++ main.js
@@ -0,0 +1,18 @@
+const km = prompt( "Quanti km vuoi percorrere?" )
+console.log(km)
+
+const eta = prompt( "Quanti anni hai?" )
+console.log(eta)
+
+const ticket = parseFloat(0.21)
+
+const sconto_1 = parseFloat
+
+const sconto_2 = parseFloat
+
+if(eta < 18) {
+    sconto_1
+}
+else if(eta >= 18) {
+    sconto_2 
+}
